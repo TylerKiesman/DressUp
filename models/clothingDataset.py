6a1e052a80a4176a07ff52777392cec6a9b9b9dc
Tyler Kiesman <tylerkiesman@gmail.com>
2020-03-03 18:40:56 -0500

    Added in the initial file for creating a dataset to be used for training the model.

---
+++ models/clothingDataset.py
@@ -0,0 +1,63 @@
+import os
+import numpy as np
+import torch
+import re
+from PIL import Image
+from xml.dom import minidom
+
+
+class ClothingDataset(object):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.images = list(sorted(os.listdir(os.path.join(root, "Clothing_Data"))))
+
+    def __getitem__(self, idx):
+        # load images ad masks
+        img_path = os.path.join(self.root, "Clothing_Data", self.images[idx])
+        img = Image.open(img_path).convert("RGB")
+        # note that we haven't converted the mask to RGB,
+        # because each color corresponds to a different instance
+        # with 0 being background
+        xml_path = re.sub("\..*", ".xml", img_path)
+
+        xml_file = minidom.parse(xml_path)
+
+        objects = xml_file.getElementsByTagName('object')
+
+        num_objs = len(objects)
+        boxes = []
+        for obj in objects:
+            bndbox = obj["bndbox"]
+            xmin = np.min(bndbox["xmin"])
+            xmax = np.max(bndbox["xmax"])
+            ymin = np.min(bndbox["ymin"])
+            ymax = np.max(bndbox["ymax"])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.images)
