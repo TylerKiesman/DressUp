01166009d05d13425b7e1648403ee2dc2bfcdfc6
Tyler Kiesman <tylerkiesman@gmail.com>
2020-03-03 19:44:05 -0500

    Added in code to return a pretrained model that can be altered for predicting different classes

---
+++ models/customModel.py
@@ -0,0 +1,14 @@
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+
+def get_model_instance_segmentation(num_classes):
+    # load an instance segmentation model pre-trained pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    return model
